590674ec49a4f49997eb6962823a439d3768ac12
Viktar Kazlou <KazlouV@iba>
2017-09-12 18:13:55 +0300

    -

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
     constructor() {}
 
-    setRange(min, max) {
+        
+   
 
+    setRange(min, max) {
+       this.min = min ;
+       this.max = max ;     
     }
 
     guess() {
-
+        return parseInt(((this.min + this.max )/2));
     }
 
     lower() {
-
+        this.max = (this.min + this.max)/2 + 1;
     }
 
     greater() {
-
+        this.min = (this.min + this.max)/2 - 1;
     }
 }
 
